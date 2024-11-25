9c20c2bdd353361b6c5f2742c6d933296f8224eb
Seok_Soo <whdtn7855@naver.com>
2024-11-25 23:41:43 +0900

    카드 GUI 세팅

--- src/OneCardGameGUI.java
+++ src/OneCardGameGUI.java
@@ -24,12 +24,20 @@ public class OneCardGameGUI extends JPanel {
         cardDeckButton = new JButton("카드 뭉치");
         oneCardButton = new JButton("원카드!");
         oneCardButton.setEnabled(false); // 기본적으로 비활성화
+
         playerNameLabels = new JLabel[4];
-        for (int i = 0; i < playerNameLabels.length; i++) {
-            playerNameLabels[i] = new JLabel("Player " + (i + 1));
-            playerNameLabels[i].setBounds(50 + i * 250, 10, 200, 30); // 적절한 위치 설정
-            add(playerNameLabels[i]);
+        addPlayerLabels(playerNameLabels);
+
+        // 카드 버튼 생성 / 나중에 수정할 부분. 플레이어 별로 카드가 생성되게 해야됨
+        JButton[][] cardButtons = new JButton[4][16];
+        for (int i = 0; i < cardButtons.length; i++) {
+            for (int j = 0; j < cardButtons[i].length; j++) {
+                cardButtons[i][j] = new JButton("Card " + (j + 1));
+            }
         }
+
+        // 카드 UI 생성
+        handCardUI(cardButtons);
 
         deckSizeLabel = new JLabel("남은 카드: " + game.getDeck().size());
         deckSizeLabel.setBounds(1100, 10, 150, 30);
@@ -39,6 +47,33 @@ public class OneCardGameGUI extends JPanel {
         initializeGame(); // 게임 로직 초기화
         // addPlayerCards();
         // addListeners();
+    }
+
+    private void addPlayerLabels(JLabel[] playerLabels) {
+        for (int i = 0; i < playerLabels.length; i++) {
+            playerLabels[i] = new JLabel("Player " + (i + 1));
+
+            // 위치 설정
+            switch (i) {
+                case 0: // 왼쪽 위 모서리
+                    playerLabels[i].setBounds(10, 10, 200, 30);
+                    break;
+                case 1: // 오른쪽 위 모서리
+                    playerLabels[i].setBounds(1000, 10, 200, 30); // 프레임 크기에 맞춰 x 좌표 수정
+                    break;
+                case 2: // 오른쪽 아래 모서리
+                    playerLabels[i].setBounds(1000, 700, 200, 30); // 프레임 크기에 맞춰 y 좌표 수정
+                    break;
+                case 3: // 왼쪽 아래 모서리
+                    playerLabels[i].setBounds(10, 700, 200, 30);
+                    break;
+                default:
+                    break;
+            }
+            add(playerLabels[i]);
+
+        }
+
     }
 
     private void initializeGame() {
@@ -56,14 +91,50 @@ public class OneCardGameGUI extends JPanel {
         repaint(); // UI 갱신
     }
 
+    private void handCardUI(JButton[][] cardButtons) {
+
+        int overlapOffset = 25;
+        int cardWidth = 100;
+        int cardHeight = 150;
+
+        for (int i = 0; i < cardButtons.length; i++) {
+            for (int j = 0; j < cardButtons[i].length; j++) {
+
+                JButton cardButton = cardButtons[i][j];
+                switch (i) {
+                    case 0: // 왼쪽 위 모서리
+                        cardButtons[i][j].setBounds(50 + j * overlapOffset, 50, cardWidth, cardHeight);
+                        break;
+                    case 1: // 오른쪽 위 모서리
+                        cardButtons[i][j].setBounds(700 + j * overlapOffset, 50, cardWidth, cardHeight); // 프레임 크기에 맞춰 x
+                                                                                                         // 좌표 수정
+                        break;
+                    case 2: // 오른쪽 아래 모서리
+                        cardButtons[i][j].setBounds(700 + j * overlapOffset, 550, cardWidth, cardHeight); // 프레임 크기에 맞춰
+                                                                                                          // y 좌표 수정
+                        break;
+                    case 3: // 왼쪽 아래 모서리
+                        cardButtons[i][j].setBounds(50 + j * overlapOffset, 550, cardWidth, cardHeight);
+                        break;
+                    default:
+                        break;
+                }
+                // cardButton.setBounds(baseX + i * overlapOffset, baseY, cardWidth,
+                // cardHeight);
+                this.add(cardButton); // 카드 패널에 추가
+            }
+        }
+
+    }
+
     private void addCentralUI() {
         int rectWidth = 500;
         int rectHeight = 300;
         int rectX = (1200 - rectWidth) / 2;
         int rectY = (800 - rectHeight) / 2;
 
-        submittedCardButton.setBounds(rectX + 40, rectY + 40, 150, 200);
-        cardDeckButton.setBounds(rectX + rectWidth - 190, rectY + 40, 150, 200);
+        submittedCardButton.setBounds(rectX + 110, rectY + 40, 100, 150);
+        cardDeckButton.setBounds(rectX + rectWidth - 220, rectY + 40, 100, 150);
         oneCardButton.setBounds(rectX + rectWidth / 2 - 50, rectY + rectHeight - 60, 100, 50);
 
         this.add(submittedCardButton);
